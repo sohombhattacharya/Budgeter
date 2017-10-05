53cf415f6c428e1a6f03974fb3083c4d208df0d3
sohombhattacharya <sohom95@yahoo.com>
2017-10-05 19:44:06 -0400

    got rid of calling bind in render ** bad practice **, changed to initializing bindings in constructor for ToDoForm

--- app/src/components/ToDoForm.js
+++ app/src/components/ToDoForm.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 class ToDoForm extends React.Component {
-    create(event) {
+    
+    constructor(){
+        super();
+        this.createNewItem = this.createNewItem.bind(this);
+    }
+    
+    createNewItem(event) {
         event.preventDefault();
         var description = this.refs.newItemDescription.value;
         var price = this.refs.newItemPrice.value;
@@ -12,7 +18,7 @@ class ToDoForm extends React.Component {
     }
     render(){
         return (
-            <form onSubmit={this.create.bind(this)}>
+            <form onSubmit={this.createNewItem}>
                 <input type="text" ref="newItemDescription" value="" required><input type="text" ref="newItemPrice" value="" required><button type="submit">Add</button>
             </form>            
             
---
+++ app/src/components/ToDoList.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+class ToDoList extends React.Component {
+    
+}
