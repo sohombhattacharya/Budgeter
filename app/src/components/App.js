5918e5d01422be2134f7ebf2ac731a7f1b5af6f6
sohombhattacharya <sohom95@yahoo.com>
2017-10-06 08:10:30 -0400

    added Budget.js

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            items: []
+            items: [],
+            budget: 0.00
         };
         this.createNewItem = this.createNewItem.bind(this);
     }
